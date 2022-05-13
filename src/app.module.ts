e14c9283bb12786b6e0c38572663b2130dfe58bb
민스톤_김민석 <sjms321@afreecatv.com>
2022-05-13 17:40:00 +0900

    delete cat create user (test commit)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [CatsModule],
+  imports: [CatsModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
