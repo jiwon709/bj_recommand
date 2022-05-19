cacf8a8c8e34a7b89b2cf25dd2600c0ba1203a91
민스톤_김민석 <sjms321@afreecatv.com>
2022-05-19 16:26:06 +0900

    DB연결 구성

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { EmailModule } from './email/email.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './user/entities/user.entity';
 
 @Module({
   imports: [
@@ -12,7 +14,20 @@ import { ConfigModule } from "@nestjs/config";
       envFilePath: '.env',
       isGlobal:true
     }),
-    ConfigModule
+    ConfigModule,
+    TypeOrmModule.forRoot(
+      {
+        type : 'mariadb',
+        host : process.env.DATABASE_HOST,
+        port : 3306,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: 'test',
+        entities : [__dirname + '/**/*.entity{.ts,.js}'],
+        // entities:[UserEntity],
+        // synchronize : Boolean(process.env.DATABASE_SYNCHRONIZE), //서비스 구동시 소스코드 기반으로 데이터베이스 스키마를 동기화 할 지
+      }
+    )
   ],
   controllers: [AppController],
   providers: [AppService],
